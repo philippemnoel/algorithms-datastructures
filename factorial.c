c0589b04d58958a6d8cc4592d65aeff0acf06f21
Philippe Noel <philippe_noel@college.harvard.edu>
2017-12-23 19:52:29 -0500

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int factorial(int n) {
+  if (n < 1) {
+    printf("%i\n", 1);
+    return 1;
+  }
+  else {
+    printf("%i\n", n);
+    return n * factorial(n - 1);
+  }
+}
+
+
+int main() {
+  printf("%i\n", factorial(6));
+  return 0;
+}
